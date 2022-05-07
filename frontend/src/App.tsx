cb67d364840bc576ebda38e7982518ee21fbdc5c
jumpei00 <61613314+jumpei00@users.noreply.github.com>
2022-05-07 17:44:25 +0900

    ヘッダーの作成が完了

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/Theme";
-import { ThreadDetail } from "./pages/threadDetail/Page";
-import { SingUp } from "./pages/singup/Page";
-// import { Home } from "./pages/home/Page";
+import { Header } from "./components/templates/header/Header";
 
 const App: React.FC = () => {
     return (
         <ChakraProvider theme={theme}>
-            {/* <Home></Home> */}
-            {/* <ThreadDetail
-                hashID="1"
-                title="test"
-                contributer="motohashi"
-                postDate="2020/1/1 12:00:00"
-                updateDate="2022/1/1 13:00"
-                views={10}
-                sumComment={20}
-            ></ThreadDetail> */}
-            <SingUp></SingUp>
+            <Header></Header>
         </ChakraProvider>
     );
 };
---
+++ frontend/src/components/templates/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Heading, Flex, Spacer } from "@chakra-ui/react";
+import { HeaderNavigation } from "../../organisms/navigation/headerNavigation";
+
+export const Header: React.FC = () => {
+    return (
+        <Box p="30px" bg="gray.300">
+            <Flex>
+                <Heading>6ちゃんねる</Heading>
+                <Spacer></Spacer>
+                <HeaderNavigation></HeaderNavigation>
+            </Flex>
+        </Box>
+    );
+};

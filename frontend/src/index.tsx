4c84c09839abca696af63909b1bacaee7a617e78
jumpei00 <61613314+jumpei00@users.noreply.github.com>
2022-05-11 22:18:48 +0900

    reviewdogのテスト

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+const hoge = "hoge";
+const state = {
+    hoge: hoge
+}
+console.log(state)
+
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(

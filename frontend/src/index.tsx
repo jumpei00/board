84b87fe55453a86986a37be86c39ee7e4adb4ca1
jumpei00 <61613314+jumpei00@users.noreply.github.com>
2022-05-11 23:27:42 +0900

    reviewdogの設定の最終決定

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const num = 1
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(

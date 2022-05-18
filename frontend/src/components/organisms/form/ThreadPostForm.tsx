aea3ef925181ecefe5cb529befe59e01b2e1ee84
jumpei00 <61613314+jumpei00@users.noreply.github.com>
2022-05-19 00:38:46 +0900

    threadの新規作成部分の機能追加

--- frontend/src/components/organisms/form/ThreadPostForm.tsx
+++ frontend/src/components/organisms/form/ThreadPostForm.tsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
+import { useDispatch } from "react-redux";
 import { Text, Input, Box } from "@chakra-ui/react";
 import { PrimaryButton } from "../../atoms/button/PrimaryButton";
+import { createThreadPayload } from "../../../pages/home/reducks/threads/type";
+import { createThread } from "../../../pages/home/reducks/threads";
 
 export const ThreadPostForm: React.FC = () => {
+    const dispatch = useDispatch();
+    const [value, setValue] = useState("");
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    const createThreadPayload: createThreadPayload = {
+        title: value,
+        contributer: "ゲスト",
+    };
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const currentValue = event.target.value;
+        currentValue === "" ? setButtonDisabled(true) : setButtonDisabled(false);
+        setValue(currentValue);
+    };
+
+    const threadPostByButtonClick = () => {
+        dispatch(createThread(createThreadPayload));
+        setValue("");
+    };
+
     return (
         <Box w="70%" m="auto">
             <Text mb="10px">スレッドタイトル</Text>
-            <Input type="text" size="lg" mb="10px" variant="flushed" placeholder="話題を投稿しましょう"></Input>
-            <PrimaryButton colorScheme="teal">スレッドを投稿</PrimaryButton>
+            <Input
+                type="text"
+                size="lg"
+                mb="10px"
+                variant="flushed"
+                placeholder="話題を投稿しましょう"
+                value={value}
+                onChange={handleChange}
+            ></Input>
+            <PrimaryButton colorScheme="teal" onClick={threadPostByButtonClick} isDisabled={buttonDisabled}>
+                スレッドを投稿
+            </PrimaryButton>
         </Box>
     );
 };
--- frontend/src/pages/home/Page.tsx
+++ frontend/src/pages/home/Page.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { VisitorStat } from "../../components/organisms/stat/VisitorStat";
 import { ThreadPostForm } from "../../components/organisms/form/ThreadPostForm";
 import { ThreadsBoardList } from "../../components/templates/threads/ThreadBoardList";
 import { RootState } from "../../store/store";
+import { getAllThread } from "./reducks/threads";
+import { getVisitors } from "./reducks/visitors";
 
 export const Home: React.FC = () => {
     const visitors = useSelector((state: RootState) => state.visitors);
     const threads = useSelector((state: RootState) => state.threads.threads);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllThread());
+    }, []);
+
+    useEffect(() => {
+        dispatch(getVisitors());
+    }, []);
 
     return (
         <>

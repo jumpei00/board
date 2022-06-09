bd2a5b560230efdce8d3f205071f56148f2129aa
jumpei00 <61613314+jumpei00@users.noreply.github.com>
2022-06-09 23:45:29 +0900

    visitor層の構築

--- backend/main.go
+++ backend/main.go
@@ -13,26 +13,31 @@ func main() {
 	router := gin.Default()
 
 	apigroup := router.Group("/api")
+	visitorGroup := router.Group("/api/visitor")
 	threadGroup := router.Group("/api/thread")
 	commentGroup := router.Group("/api/comment")
 
 	// DB
 	userDB := infrastructure.NewUserDB()
+	visitorDB := infrastructure.NewVisitorDB()
 	threadDB := infrastructure.NewThreadDB()
 	commentDB := infrastructure.NewCommentDB()
 
 	// application
 	userApp := application.NewUserApplication(userDB)
+	visitApp := application.NewVisitorApplication(visitorDB)
 	threadApp := application.NewThreadApplication(threadDB)
 	commentApp := application.NewCommentApplication(threadDB, commentDB)
 
 	// handler
 	userHandler := interfaces.NewUserHandler(userApp)
+	visitorHandler := interfaces.NewVisitorsHandler(visitApp)
 	threadHandler := interfaces.NewThreadHandler(threadApp)
 	commentHandler := interfaces.NewCommentHandler(threadApp, commentApp)
 
 	// router setup
 	userHandler.SetupRouter(apigroup)
+	visitorHandler.SetupRouter(visitorGroup)
 	threadHandler.SetupRouter(threadGroup)
 	commentHandler.SetupRouter(commentGroup)
 

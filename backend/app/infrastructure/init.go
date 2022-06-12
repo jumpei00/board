8e37b845f2dc1e2f5f7a1a137a5c844297d95af8
jumpei00 <61613314+jumpei00@users.noreply.github.com>
2022-06-12 23:26:25 +0900

    dbセッションの作成

---
+++ backend/app/infrastructure/init.go
@@ -0,0 +1,36 @@
+package infrastructure
+
+import (
+	"fmt"
+
+	"github.com/jumpei00/board/backend/app/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func GenerateDBSession() (*gorm.DB, error) {
+	// dsn -> username:password@protocol(host:port)/dbname?param=value
+	dsn := fmt.Sprintf(
+		"%s:%s@%s(%s:%s)/%s?charset=utf8mb4&parseTime=True",
+		config.GetMySQLUserName(),
+		config.GetMySQLPassword(),
+		"tcp",
+		config.GetMySQLHost(),
+		config.GetMySQLPort(),
+		config.GetMySQLDatabaseName(),
+	)
+
+	dbLogger := logger.Default
+	// 本番環境の場合はDBのログを出力しない
+	if config.IsProduction() {
+		dbLogger.LogMode(logger.Silent)
+	}
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: dbLogger})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- backend/go.mod
+++ backend/go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/pkg/errors v0.9.1
 	go.uber.org/zap v1.21.0
 	golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/gorm v1.23.6
 )
 
 require (
@@ -14,7 +16,10 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect

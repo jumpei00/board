c09d53164ae490250e00ade20b5ece186ea4a67b
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2022-07-07 00:07:19 +0900

    userのリクエスト部分のリファクタ

--- backend/app/interfaces/init_test.go
+++ backend/app/interfaces/init_test.go
@@ -8,9 +8,7 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	appError "github.com/jumpei00/board/backend/app/library/error"
 	"github.com/jumpei00/board/backend/app/library/logger"
-	"github.com/pkg/errors"
 )
 
 func TestMain(m *testing.M) {
@@ -29,23 +27,4 @@ func executeHttpTest(r *gin.Engine, method string, path string, body io.Reader) 
 	r.ServeHTTP(responseRecorder, request)
 
 	return responseRecorder
-}
-
-func isSameError(e1, e2 error) bool {
-	e1 = errors.Cause(e1)
-	e2 = errors.Cause(e2)
-
-	if e1 == nil && e2 == nil {
-		return true
-	} else if e1 != nil && e2 == nil {
-		return false
-	} else if e1 == nil && e2 != nil {
-		return false
-	}
-
-	if appError.IsBadRequest(e1) && appError.IsBadRequest(e2) {
-		return true
-	}
-
-	return e1 == e2
 }

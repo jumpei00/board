0d2a2f08832ea6ff84c05ab6fdda64e581aeba49
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2022-07-24 00:57:28 +0900

    エンドポイントやaxiosの設定修正

--- frontend/src/api/init.ts
+++ frontend/src/api/init.ts
@@ -1,7 +1,27 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { camelizeKeys, decamelizeKeys } from "humps";
 import { ApiURL } from "../config/env";
 
-export const boardApi = axios.create({
+const boardApi = axios.create({
     baseURL: ApiURL,
-    withCredentials: true
+    withCredentials: true,
+});
+
+boardApi.interceptors.request.use((config: AxiosRequestConfig) => {
+    if (config.params) {
+        config.params = decamelizeKeys(config.params);
+    }
+    if (config.data) {
+        config.data = decamelizeKeys(config.data);
+    }
+    return config;
+});
+
+boardApi.interceptors.response.use((response: AxiosResponse) => {
+    if (response.data && response.headers["content-type"].match("application/json")) {
+        response.data = camelizeKeys(response.data)
+    }
+    return response
 })
+
+export default boardApi;

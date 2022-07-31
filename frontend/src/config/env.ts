0ffd173ee1df468792bb07243868b37415728133
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2022-07-31 11:16:54 +0900

    reactの本番環境エンドポイントを設定

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -8,7 +8,7 @@ export const ENV: envType = process.env.REACT_APP_WEB_ENV as envType;
 
 const urlMap: envMap = {
     development: "http://api.localhost.test",
-    production: "http://api.localhost.test",
+    production: "https://board-api-service-2x4i4vgx5q-an.a.run.app",
 };
 
 export const ApiURL = urlMap[ENV]
